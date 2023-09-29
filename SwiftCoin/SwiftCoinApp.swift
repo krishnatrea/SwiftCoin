5734ae08ab79488a07d8b16435d89b2e41ead89a
krishnatrea <krishnatrea@gmail.com>
2023-09-29 21:21:36 +0530

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Krishnatrea on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
